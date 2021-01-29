bd586a03036c52684265c273f73cc8225e68f2af
william <wsta@kth.se>
2021-01-29 16:41:27 +0100

    added maven

---
+++ src/main/java/se/kth/iv1201/group4/App.java
@@ -0,0 +1,11 @@
+package se.kth.iv1201.group4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
