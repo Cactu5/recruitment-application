d36aa3e3de30edf09a3953491f5f1f023dfc79b0
Filip Garamvölgyi <filipgar@kth.se>
2021-02-03 01:59:47 +0100

    added basic SpringWebMVC configs

---
+++ src/main/java/se/kth/iv1201/group4/recruitment/config/RecruitmentConfig.java
@@ -0,0 +1,104 @@
+package se.kth.iv1201.group4.recruitment.config;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+
+/**
+ * Contains configurations for SpringMVC settings.
+ *
+ * @author Filip Garamvölgyi
+ */
+@EnableWebMvc
+@Configuration
+//@ComponentScan({"se.kth.iv1201.group4.recruitment"})
+public class RecruitmentConfig implements WebMvcConfigurer, ApplicationContextAware {  
+    public static final String TEMPLATE_FILE_LOCATION = "classpath:/templates/";
+    public static final String STATIC_RESOURCES_LOCATION = "classpath:/web-root/";
+    public static final int CHACHE_PERIOD_FOR_STATIC_FILES_SEC = 1;
+
+    private ApplicationContext applicationContext;
+
+    /**
+     * @param applicationContext    applicationContext used for the backend
+     *                              of the webserver.
+     */
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+    /** 
+     * Makes sure that every view of the web server will be resolved by 
+     * {@link ThymeleafViewResolver} also sets the meta data of the view resolver
+     * 
+     * @return {@link ThymeleafViewResolver}    contains the settins set
+     */ 
+    @Bean 
+    public ThymeleafViewResolver viewResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setCharacterEncoding("UTF-8");
+        viewResolver.setContentType("text/html; charset=UTF-8");
+        viewResolver.setTemplateEngine(templateEngine());
+        return viewResolver;
+    }
+
+    /**
+     * Configuration for Thymeleaf's template engine integration with spring.
+     *
+     * @return {@link SpringTemplateEngine}    used for all template resolutions.
+     */
+    @Bean(name = "recruitmentTemplateEngine")
+    public SpringTemplateEngine templateEngine(){
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setEnableSpringELCompiler(true);
+        templateEngine.addDialect(new LayoutDialect());
+        return templateEngine;
+    }
+
+    /**
+     * Configurations to intergrate Thymeleaf with Spring
+     *
+     * @return {@link SpringResourceTemplateResolver}   Only template resolver to 
+     *                                                  be used by the web server
+     */
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = 
+            new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(applicationContext);        
+        templateResolver.setPrefix(TEMPLATE_FILE_LOCATION);
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCacheable(false);
+
+        return templateResolver;
+    }
+    
+    /**
+     * Configure static resources to be accessed by web clients.
+     *
+     * @param registry    used to configure static resources
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+            .addResourceLocations(STATIC_RESOURCES_LOCATION)
+            .setCachePeriod(CHACHE_PERIOD_FOR_STATIC_FILES_SEC)
+            .resourceChain(true)
+            .addResolver(new PathResourceResolver());
+    }
+}
---
+++ src/test/java/se/kth/iv1201/group4/recruitment/config/RecruitmentConfigTest.java
@@ -0,0 +1,79 @@
+package se.kth.iv1201.group4.recruitment.config;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.TestExecutionListener;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+@SpringJUnitWebConfig(initializers = ConfigDataApplicationContextInitializer.class)
+@EnableAutoConfiguration
+@ComponentScan(basePackages = {"se.kth.iv1201.group4.recruitment"})
+@TestExecutionListeners(listeners = { DependencyInjectionTestExecutionListener.class, RecruitmentConfigTest.class  })
+class RecruitmentConfigTest implements TestExecutionListener {
+    static final String STATIC_RESOURCES_TEST_LOCATION
+        = "/resource/TO_BE_ADDED";
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setup() throws Exception {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @Disabled
+    @Test
+    void testStaticResourcesCanBeAccessed() throws Exception {
+        mockMvc.perform( MockMvcRequestBuilders.get(STATIC_RESOURCES_TEST_LOCATION))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andReturn()
+            .getResponse()
+            .getContentType()
+            .equalsIgnoreCase("CONTENT TYPE ENTERED HERE");
+    }
+    @Test
+    void testCorrectTemplateEngineIsUsed(@Autowired @Qualifier("recruitmentTemplateEngine") 
+            SpringTemplateEngine templateEngine,
+            @Autowired ThymeleafViewResolver viewResolver){
+        System.out.println("Test correct template engine is being used");
+        assertEquals(
+            templateEngine, viewResolver.getTemplateEngine(), 
+            "configured template engine is not used..."
+        );
+    }
+
+    @Test
+    void testCorrectTemplateResolverIsUsed(@Autowired ThymeleafViewResolver viewResolver,
+            @Autowired SpringResourceTemplateResolver templateResolver){
+        System.out.println("Test correct template resolver is being used");
+        System.out.println(templateResolver);
+        ((SpringTemplateEngine) viewResolver.getTemplateEngine())
+            .getTemplateResolvers()
+            .stream()
+            .forEach(System.out::println);
+        assertTrue(
+                ((SpringTemplateEngine) viewResolver.getTemplateEngine())
+                .getTemplateResolvers()
+                .contains(templateResolver), "configured template resolver is not used...");
+    }
+}
