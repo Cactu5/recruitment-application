4b1cd1f44036f65c785440164181451e78e96051
Cactu5 <cactusdevcode@gmail.com>
2021-02-04 10:04:09 +0100

    Add skeleton of domain classes

---
+++ src/main/java/se/kth/iv1201/group4/recruitment/domain/CompetenceProfile.java
@@ -0,0 +1,20 @@
+package se.kth.iv1201.group4.recruitment.domain;
+
+import se.kth.iv1201.group4.recruitment.dto.CompetenceDTO;
+import se.kth.iv1201.group4.recruitment.dto.CompetenceProfileDTO;
+
+public class CompetenceProfile implements CompetenceProfileDTO {
+
+    @Override
+    public CompetenceDTO getCompetence() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public float getYearsOfExperience() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
---
+++ src/main/java/se/kth/iv1201/group4/recruitment/domain/JobStatus.java
@@ -0,0 +1,13 @@
+package se.kth.iv1201.group4.recruitment.domain;
+
+import se.kth.iv1201.group4.recruitment.dto.JobStatusDTO;
+
+public class JobStatus implements JobStatusDTO {
+
+    @Override
+    public String getName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
