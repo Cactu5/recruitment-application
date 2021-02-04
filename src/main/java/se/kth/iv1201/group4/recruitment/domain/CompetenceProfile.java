2662c211bf4196c15b5136ca28bd10785f0e5f0e
Cactu5 <cactusdevcode@gmail.com>
2021-02-04 15:57:36 +0100

    Add domains

--- src/main/java/se/kth/iv1201/group4/recruitment/domain/CompetenceProfile.java
+++ src/main/java/se/kth/iv1201/group4/recruitment/domain/CompetenceProfile.java
@@ -1,20 +1,82 @@
 package se.kth.iv1201.group4.recruitment.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.ToString;
 import se.kth.iv1201.group4.recruitment.dto.CompetenceDTO;
 import se.kth.iv1201.group4.recruitment.dto.CompetenceProfileDTO;
 
+@ToString
+@Entity
+@Table(name = "competence_profile")
 public class CompetenceProfile implements CompetenceProfileDTO {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "years_of_experience")
+    private float yearsOfExperience;
+
+    @ManyToOne
+    @JoinColumn(name = "competence")
+    private Competence competence;
+
+    @ManyToOne
+    @JoinColumn(name = "job_application")
+    private JobApplication jobApplication;
+
+    /**
+     * Required by JPA
+     */
+    protected CompetenceProfile() {
+
+    }
+
+    /**
+     * Creates a new instance with the specified years of experience, competence and
+     * job application.
+     * 
+     * @param yearsOfExperience
+     * @param competence
+     * @param jobApplication
+     */
+    public CompetenceProfile(float yearsOfExperience, Competence competence, JobApplication jobApplication) {
+        this.yearsOfExperience = yearsOfExperience;
+        this.competence = competence;
+        this.jobApplication = jobApplication;
+    }
 
     @Override
     public CompetenceDTO getCompetence() {
-        // TODO Auto-generated method stub
-        return null;
+        return competence;
     }
 
     @Override
     public float getYearsOfExperience() {
-        // TODO Auto-generated method stub
-        return 0;
+        return yearsOfExperience;
+    }
+
+    @Override
+    public int hashCode() {
+        return Long.valueOf(id).hashCode();
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof CompetenceProfile)) {
+            return false;
+        }
+        CompetenceProfile other = (CompetenceProfile) object;
+        return this.id == other.id;
     }
 
 }
--- src/main/java/se/kth/iv1201/group4/recruitment/domain/Recruiter.java
+++ src/main/java/se/kth/iv1201/group4/recruitment/domain/Recruiter.java
@@ -1,7 +1,58 @@
 package se.kth.iv1201.group4.recruitment.domain;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.ToString;
 import se.kth.iv1201.group4.recruitment.dto.RecruiterDTO;
 
+@ToString
+@Entity
+@Table(name = "recruiter")
 public class Recruiter implements RecruiterDTO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
 
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "person", referencedColumnName = "id")
+    private Person person;
+
+    /**
+     * Required by JPA
+     */
+    protected Recruiter() {
+
+    }
+
+    /**
+     * Creates a new instance with the specified person.
+     * 
+     * @param person the person.
+     */
+    public Recruiter(Person person) {
+        this.person = person;
+    }
+
+    @Override
+    public int hashCode() {
+        return Long.valueOf(id).hashCode();
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof Recruiter)) {
+            return false;
+        }
+        Recruiter other = (Recruiter) object;
+        return this.id == other.id;
+    }
 }
