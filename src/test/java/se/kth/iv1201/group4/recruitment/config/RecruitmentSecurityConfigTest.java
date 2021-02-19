c088671d8232a0a96999a2b996191fc4a4422cc2
Cactu5 <cactusdevcode@gmail.com>
2021-02-19 11:08:11 +0100

    Add more tests

--- src/test/java/se/kth/iv1201/group4/recruitment/config/RecruitmentSecurityConfigTest.java
+++ src/test/java/se/kth/iv1201/group4/recruitment/config/RecruitmentSecurityConfigTest.java
@@ -19,40 +19,44 @@ import org.springframework.web.context.WebApplicationContext;
 
 @SpringJUnitWebConfig(initializers = ConfigDataApplicationContextInitializer.class)
 @EnableAutoConfiguration
-@ComponentScan(basePackages = {"se.kth.iv1201.group4.recruitment"})
-@TestExecutionListeners(listeners = { DependencyInjectionTestExecutionListener.class, RecruitmentSecurityConfigTest.class })
-class RecruitmentSecurityConfigTest implements TestExecutionListener{
+@ComponentScan(basePackages = { "se.kth.iv1201.group4.recruitment" })
+@TestExecutionListeners(listeners = { DependencyInjectionTestExecutionListener.class,
+        RecruitmentSecurityConfigTest.class })
+class RecruitmentSecurityConfigTest implements TestExecutionListener {
     @Autowired
     private WebApplicationContext context;
 
     private MockMvc mockMvc;
-    
+
     @BeforeEach
     void setup() throws Exception {
-        mockMvc = MockMvcBuilders
-            .webAppContextSetup(context)
-            .apply(SecurityMockMvcConfigurers.springSecurity())
-            .build();
-    }
-   
-    @Test
-    void testContentProtectedBehindAuthentication() throws Exception{
-        mockMvc.perform(MockMvcRequestBuilders.get("/nonexistingpage"))
-            .andExpect(MockMvcResultMatchers.status().is3xxRedirection());
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).apply(SecurityMockMvcConfigurers.springSecurity())
+                .build();
     }
 
     @Test
-    void testLoginDoesNotRequireAuthentication() throws Exception{
+    void testContentProtectedBehindAuthentication() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/success-applicant"))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection());
+    }
+
+    @Test
+    void testPageNotFound() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/dfk9d84jgdijgofdkg"))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    void testLoginDoesNotRequireAuthentication() throws Exception {
         pageIsOk("/login");
     }
 
     @Test
-    void testRegisterDoesNotRequireAuthentication() throws Exception{
+    void testRegisterDoesNotRequireAuthentication() throws Exception {
         pageIsOk("/register");
     }
 
-    private void pageIsOk(String page) throws Exception{
-        mockMvc.perform(MockMvcRequestBuilders.get(page))
-            .andExpect(MockMvcResultMatchers.status().isOk());
+    private void pageIsOk(String page) throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get(page)).andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
