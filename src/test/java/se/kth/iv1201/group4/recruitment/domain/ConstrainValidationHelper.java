59225b583b9cabe06b581960e9dc9a8806f28391
Cactu5 <cactusdevcode@gmail.com>
2021-02-25 11:56:29 +0100

    Fix PersonTests and constraint validator

---
+++ src/test/java/se/kth/iv1201/group4/recruitment/domain/ConstrainValidationHelper.java
@@ -0,0 +1,39 @@
+package se.kth.iv1201.group4.recruitment.domain;
+
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.hasItem;
+
+/**
+ * This is class with helper functions for validating constraints.
+ * 
+ * @author Leif Lindbäck
+ * @author Cactu5
+ * @version %I%
+ */
+public class ConstrainValidationHelper {
+    /**
+     * This is a helper function that helps validate
+     * <code>ConstraintViolationException</code>.
+     * 
+     * Original from Leif Lindbäck
+     * https://github.com/KTH-IV1201/bank/blob/15c1e687c76e7ccc203ef998f7e2584f28877e0b/src/test/java/se/kth/iv1201/appserv/bank/domain/AccountTest.java#L143
+     * 
+     * @param e                  the exception
+     * @param expectedContraints the expected exceptions
+     */
+    public static void validateConstraints(ConstraintViolationException e, String... expectedContraints) {
+        Set<ConstraintViolation<?>> result = e.getConstraintViolations();
+        assertThat(result.size(), is(expectedContraints.length));
+        for (String expectedContraint : expectedContraints) {
+            assertThat(result, hasItem(hasProperty("messageTemplate", equalTo(expectedContraint))));
+        }
+    }
+}
--- src/test/java/se/kth/iv1201/group4/recruitment/domain/PersonTest.java
+++ src/test/java/se/kth/iv1201/group4/recruitment/domain/PersonTest.java
@@ -2,17 +2,14 @@ package se.kth.iv1201.group4.recruitment.domain;
 
 import static org.hamcrest.Matchers.is;
 
-import java.util.Set;
-
-import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasProperty;
-import static org.hamcrest.CoreMatchers.hasItem;
+
+import static se.kth.iv1201.group4.recruitment.domain.ConstrainValidationHelper.validateConstraints;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.fail;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
@@ -31,8 +28,7 @@ public class PersonTest implements TestExecutionListener {
     @Test
     public void testCreatePerson() {
         Person ben = new Person("Ben", "Johnsson", "ben.johnsson@gmail.com", "190607071234", "benjo", "password");
-        entityManager.persist(ben);
-        entityManager.flush();
+        entityManager.persistAndFlush(ben);
 
         Person person = personRepository.findPersonByUsername(ben.getUsername());
         assertThat(person, is(ben));
@@ -48,6 +44,7 @@ public class PersonTest implements TestExecutionListener {
         Person ben = new Person("!", "Johnsson", "ben.johnsson@gmail.com", "190607071234", "benjo", "password");
         try {
             entityManager.persistAndFlush(ben);
+            fail("ConstraintViolationException was not thrown.");
         } catch (ConstraintViolationException e) {
             validateConstraints(e, "{person.name.invalid-char}", "{person.name.length}");
         }
@@ -58,6 +55,7 @@ public class PersonTest implements TestExecutionListener {
         Person ben = new Person("Ben", "!", "ben.johnsson@gmail.com", "190607071234", "benjo", "password");
         try {
             entityManager.persistAndFlush(ben);
+            fail("ConstraintViolationException was not thrown.");
         } catch (ConstraintViolationException e) {
             validateConstraints(e, "{person.surname.invalid-char}", "{person.surname.length}");
         }
@@ -69,6 +67,7 @@ public class PersonTest implements TestExecutionListener {
                 "190607071234", "benjo", "password");
         try {
             entityManager.persistAndFlush(ben);
+            fail("ConstraintViolationException was not thrown.");
         } catch (ConstraintViolationException e) {
             validateConstraints(e, "{person.email.invalid}", "{person.email.length}");
         }
@@ -79,6 +78,7 @@ public class PersonTest implements TestExecutionListener {
         Person ben = new Person("Ben", "Johnsson", "ben.johnsson@gmail.com", "!", "benjo", "password");
         try {
             entityManager.persistAndFlush(ben);
+            fail("ConstraintViolationException was not thrown.");
         } catch (ConstraintViolationException e) {
             validateConstraints(e, "{person.ssn.invalid-char}", "{person.ssn.length}");
         }
@@ -89,18 +89,9 @@ public class PersonTest implements TestExecutionListener {
         Person ben = new Person("Ben", "Johnsson", "ben.johnsson@gmail.com", "190607071234", "!", "password");
         try {
             entityManager.persistAndFlush(ben);
+            fail("ConstraintViolationException was not thrown.");
         } catch (ConstraintViolationException e) {
             validateConstraints(e, "{person.username.invalid-char}", "{person.username.length}");
-        }
-    }
-
-    // Original from Leif Lindbäck
-    // https://github.com/KTH-IV1201/bank/blob/15c1e687c76e7ccc203ef998f7e2584f28877e0b/src/test/java/se/kth/iv1201/appserv/bank/domain/AccountTest.java#L143
-    private void validateConstraints(ConstraintViolationException e, String... expectedContraints) {
-        Set<ConstraintViolation<?>> result = e.getConstraintViolations();
-        assertThat(result.size(), is(expectedContraints.length));
-        for (String expectedContraint : expectedContraints) {
-            assertThat(result, hasItem(hasProperty("messageTemplate", equalTo(expectedContraint))));
         }
     }
 }
