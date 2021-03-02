14478484e88ad8094154b0aa02cb57d61b91b115
Cactu5 <cactusdevcode@gmail.com>
2021-03-02 11:35:32 +0100

    Fix RegisterView tests

--- src/test/java/se/kth/iv1201/group4/recruitment/presentation/register/RegisterViewTest.java
+++ src/test/java/se/kth/iv1201/group4/recruitment/presentation/register/RegisterViewTest.java
@@ -12,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.web.header.Header;
 import org.springframework.test.context.TestExecutionListener;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
@@ -22,10 +24,10 @@ import org.springframework.web.context.WebApplicationContext;
 
 @SpringJUnitWebConfig(initializers = ConfigDataApplicationContextInitializer.class)
 @EnableAutoConfiguration
-@ComponentScan(basePackages = {"se.kth.iv1201.group4.recruitment"})
-@TestExecutionListeners(listeners = { DependencyInjectionTestExecutionListener.class, RegisterViewTest.class  })
-class RegisterViewTest implements TestExecutionListener{
-    @Autowired 
+@ComponentScan(basePackages = { "se.kth.iv1201.group4.recruitment" })
+@TestExecutionListeners(listeners = { DependencyInjectionTestExecutionListener.class, RegisterViewTest.class })
+class RegisterViewTest implements TestExecutionListener {
+    @Autowired
     private WebApplicationContext context;
 
     private MockMvc mockMvc;
@@ -36,34 +38,44 @@ class RegisterViewTest implements TestExecutionListener{
     }
 
     @Test
-    void testIfViewRendersTheRequiredInputs() throws Exception{
-        mockMvc.perform(get("/register"))
-        .andExpect(status().isOk())
-        .andExpect(content().string(containsString("name=\"email\"")))
-        .andExpect(content().string(containsString("name=\"name\"")))
-        .andExpect(content().string(containsString("name=\"surname\"")))
-        .andExpect(content().string(containsString("name=\"username\"")))
-        .andExpect(content().string(containsString("name=\"password\"")))
-        .andExpect(content().string(containsString("name=\"SSN\"")))
-        .andExpect(content().string(containsString("type=\"submit\"")));
+    void testIfViewRendersTheRequiredInputs() throws Exception {
+        mockMvc.perform(get("/register")).andExpect(status().isOk())
+                .andExpect(content().string(containsString("name=\"email\"")))
+                .andExpect(content().string(containsString("name=\"name\"")))
+                .andExpect(content().string(containsString("name=\"surname\"")))
+                .andExpect(content().string(containsString("name=\"username\"")))
+                .andExpect(content().string(containsString("name=\"password\"")))
+                .andExpect(content().string(containsString("name=\"SSN\"")))
+                .andExpect(content().string(containsString("type=\"submit\"")));
     }
+
     @Test
-    void testIfViewRendersTextFromMessageProperties() throws Exception {
-        mockMvc.perform(get("/register"))
-        .andExpect(status().isOk())
-        .andExpect(content().string(containsString("email")))
-        .andExpect(content().string(containsString("namn")))
-        .andExpect(content().string(containsString("efternamn")))
-        .andExpect(content().string(containsString("användarnamn")))
-        .andExpect(content().string(containsString("lösenord")))
-        .andExpect(content().string(containsString("personnummer")));
+    void testIfViewRendersInSwedish() throws Exception {
+        mockMvc.perform(get("/register?lang=sv")).andExpect(status().isOk())
+                .andExpect(content().string(containsString("email")))
+                .andExpect(content().string(containsString("namn")))
+                .andExpect(content().string(containsString("efternamn")))
+                .andExpect(content().string(containsString("användarnamn")))
+                .andExpect(content().string(containsString("lösenord")))
+                .andExpect(content().string(containsString("personnummer")));
     }
+
     @Test
-    void testIfViewIsUsingThymeleafDecoration() throws Exception{
-        mockMvc.perform(get("/register"))
-        .andExpect(status().isOk())
-        .andExpect(content().string(containsString("| Registrering")))
-        .andExpect(content().string(containsString("<h1>Rekrytering</h1>")))
-        .andExpect(content().string(containsString("<footer>")));
+    void testIfViewRendersInEnglish() throws Exception {
+        mockMvc.perform(get("/register?lang=en")).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Email")))
+                .andExpect(content().string(containsString("First Name")))
+                .andExpect(content().string(containsString("Surname")))
+                .andExpect(content().string(containsString("Username")))
+                .andExpect(content().string(containsString("Password")))
+                .andExpect(content().string(containsString("Social Security Number")));
+    }
+
+    @Test
+    void testIfViewIsUsingThymeleafDecoration() throws Exception {
+        mockMvc.perform(get("/register?lang=sv")).andExpect(status().isOk())
+                .andExpect(content().string(containsString("| Registrering")))
+                .andExpect(content().string(containsString("<h1>Rekrytering</h1>")))
+                .andExpect(content().string(containsString("<footer>")));
     }
 }
