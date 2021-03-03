870bf73612ff983d59bf99cef83fa074ac67df9f
Filip Garamvölgyi <filipgar@kth.se>
2021-03-03 14:46:40 +0100

    Add test for TemporaryDataMatcher

--- src/main/java/se/kth/iv1201/group4/recruitment/util/TemporaryDataMatcher.java
+++ src/main/java/se/kth/iv1201/group4/recruitment/util/TemporaryDataMatcher.java
@@ -7,8 +7,8 @@ package se.kth.iv1201.group4.recruitment.util;
  * @author Filip Garamvölgyi
  */
 public class TemporaryDataMatcher {
-    public static final String regexUsernameMatcher = "badUserFakeUser\\d{1,5}";
-    public static final String regexEmailMatcher = "fake\\d{1,5}@fakemegafake.email";
+    public static final String regexUsernameMatcher = "badUserFakeUser\\d{5}";
+    public static final String regexEmailMatcher = "fake\\d{5}@fakemegafake.email";
 
     public static boolean isTemporaryEmail(String email){return email.matches(regexEmailMatcher);}
     public static boolean isTemporaryUsername(String username){return username.matches(regexUsernameMatcher);}
