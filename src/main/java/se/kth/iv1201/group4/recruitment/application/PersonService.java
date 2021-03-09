150482a948edc57b85d977c07e196a29081cd82d
Filip Garamvölgyi <filipgar@kth.se>
2021-03-09 11:56:47 +0100

    Add PersonService tests

--- src/main/java/se/kth/iv1201/group4/recruitment/application/PersonService.java
+++ src/main/java/se/kth/iv1201/group4/recruitment/application/PersonService.java
@@ -27,9 +27,13 @@ import se.kth.iv1201.group4.recruitment.domain.LegacyUser;
 import se.kth.iv1201.group4.recruitment.domain.Person;
 import se.kth.iv1201.group4.recruitment.domain.Recruiter;
 import se.kth.iv1201.group4.recruitment.dto.PersonDTO;
+
+import se.kth.iv1201.group4.recruitment.dto.ApplicantDTO;
 import se.kth.iv1201.group4.recruitment.repository.ApplicantRepository;
+import se.kth.iv1201.group4.recruitment.dto.LegacyUserDTO;
 import se.kth.iv1201.group4.recruitment.repository.LegacyUserRepository;
 import se.kth.iv1201.group4.recruitment.repository.PersonRepository;
+import se.kth.iv1201.group4.recruitment.dto.RecruiterDTO;
 import se.kth.iv1201.group4.recruitment.repository.RecruiterRepository;
 import se.kth.iv1201.group4.recruitment.util.TemporaryDataMatcher;
 import se.kth.iv1201.group4.recruitment.util.error.UsernameAlreadyExistsException;
@@ -76,22 +80,29 @@ public class PersonService implements UserDetailsService {
      * Adds an applicant to the applicant repository
      * 
      * @param a The applicant to add
+     * @return returns the added {@link ApplicantDTO} if successful otherwise null is returned.
      */
-    public void addApplicant(Applicant a) {
+    public ApplicantDTO addApplicant(Applicant a) {
         if (a != null) {
-            applicantRepo.saveAndFlush(a);
+            ApplicantDTO  dto = applicantRepo.saveAndFlush(a);
+            return dto;
         }
+        return null;
     }
 
     /**
      * Adds a recruiter to the recruiter repository
      * 
      * @param r The recruiter to add
+     * @return returns the added {@link RecruiterDTO} if successful otherwise null is returned.
      */
-    public void addRecruiter(Recruiter r) {
+    public RecruiterDTO addRecruiter(Recruiter r) {
         if (r != null) {
+            RecruiterDTO dto = 
             recruiterRepo.saveAndFlush(r);
+            return dto;
         }
+        return null;
     }
 
     private void removeLegacyUserByPersonDTO(PersonDTO person){
@@ -125,7 +136,7 @@ public class PersonService implements UserDetailsService {
             throw new UpdatedPersonContainsTemporaryDataException("Still contains the temporary SSN");
         }
         if(dto.getUsername() !=username &&
-                loadUserByUsername(dto.getUsername()) != null){
+                personRepo.findPersonByUsername(dto.getUsername()) != null){
             throw new UsernameAlreadyExistsException("Username is already in use.");
         }
         if(personRepo.findPersonByEmail(dto.getEmail()) != null){
@@ -141,19 +152,22 @@ public class PersonService implements UserDetailsService {
      * Adds a legacy user to the legacy user repository
      * 
      * @param lu The legacy user to add
+     * @return returns the added {@link LegacyUserDTO} if successful otherwise null is returned.
      */
-    public void addLegacyUser(LegacyUser lu) {
+    public LegacyUserDTO addLegacyUser(LegacyUser lu) {
         if (lu != null) {
-            legacyUserRepo.saveAndFlush(lu);
+            LegacyUserDTO dto = legacyUserRepo.saveAndFlush(lu);
             LOGGER.info("Added legacy user");
+            return dto;
         }
+        return null;
     }
 
     /**
      * Fetches a person from the database with the given username and password
      * 
      * @param username The username of the person to fetch
-     * @param password The password of the person to fetch
+     * @param password The hashed password of the person to fetch
      */
     public Person getPerson(String username, String password) {
         if (username == null || password == null)
@@ -165,7 +179,7 @@ public class PersonService implements UserDetailsService {
 
         if (password.equals(p.getPassword())) {
             return p;
-        }
+        } 
         return null;
     }
 
--- src/test/java/se/kth/iv1201/group4/recruitment/application/PersonServiceTest.java
+++ src/test/java/se/kth/iv1201/group4/recruitment/application/PersonServiceTest.java
@@ -1,8 +1,179 @@
 package se.kth.iv1201.group4.recruitment.application;
 
+import static org.junit.Assert.assertSame;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.doReturn;
+
+import java.util.Random;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import se.kth.iv1201.group4.recruitment.application.error.UpdatedPersonContainsTemporaryDataException;
+import se.kth.iv1201.group4.recruitment.domain.Applicant;
+import se.kth.iv1201.group4.recruitment.domain.LegacyUser;
+import se.kth.iv1201.group4.recruitment.domain.Person;
+import se.kth.iv1201.group4.recruitment.domain.Recruiter;
+import se.kth.iv1201.group4.recruitment.repository.ApplicantRepository;
+import se.kth.iv1201.group4.recruitment.repository.LegacyUserRepository;
+import se.kth.iv1201.group4.recruitment.repository.PersonRepository;
+import se.kth.iv1201.group4.recruitment.repository.RecruiterRepository;
+import se.kth.iv1201.group4.recruitment.util.error.EmailAlreadyExistsException;
+import se.kth.iv1201.group4.recruitment.util.error.UsernameAlreadyExistsException;
 
 @SpringBootTest
 public class PersonServiceTest {
+    @Autowired
+    private PersonService service;
+
+    @MockBean
+    private PersonRepository personRepo;
+
+    @MockBean
+    private ApplicantRepository applicantRepo;
+
+    @MockBean
+    private RecruiterRepository recruiterRepo;
+
+    @MockBean
+    private LegacyUserRepository legacyUserRepo;
+
+    @Test
+    void testToAddApplicant(){
+        Person p = dummyPerson();
+        Applicant a1 = new Applicant(p); 
+        doReturn(a1).when(applicantRepo).saveAndFlush(a1);
+        
+        Applicant a2 = (Applicant) service.addApplicant(a1);
+
+        assertFalse(a2 == null);
+        assertSame(a1, a2);
+    }
+
+    @Test
+    void testToAddRecruiter(){
+        Person p = dummyPerson();
+        Recruiter r1 = new Recruiter(p); 
+        doReturn(r1).when(recruiterRepo).saveAndFlush(r1);
+        
+        Recruiter r2 = (Recruiter) service.addRecruiter(r1);
+
+        assertFalse(r2 == null);
+        assertSame(r1, r2);
+    }
+
+    @Test
+    void testToAddLegacyUser(){
+        Person p = dummyPerson();
+        LegacyUser lu1 = new LegacyUser(p); 
+        doReturn(lu1).when(legacyUserRepo).saveAndFlush(lu1);
+        
+        LegacyUser lu2 = (LegacyUser) service.addLegacyUser(lu1);
+
+        assertFalse(lu2 == null);
+        assertSame(lu1, lu2);
+    }
+
+    @Test
+    void testToGetPersonByUsernameAndHashedPassword(){
+        Person p1 = dummyPerson("user","qooqWan123!");
+
+        doReturn(p1).when(personRepo).findPersonByUsername("user");
+        
+        Person p2 = service.getPerson("user", p1.getPassword());
+
+        assertFalse(p2 == null);
+        assertSame(p1, p2);
+    }
+
+    @Test
+    void testToGetApplicantByPerson(){
+        Person p = dummyPerson();
+        Applicant a1 = new Applicant(p);
+
+        doReturn(a1).when(applicantRepo).findApplicantByPerson(p);
+
+        Applicant a2 = service.getApplicant(p);
+
+        validationAssertions(a1, a2);
+    }
+
+    @Test
+    void testThatTemporarySSNCantExistWhenUpdatingPerson(){
+        Person p = dummyLegacyPerson(false, true);
+        Exception e = assertThrows(UpdatedPersonContainsTemporaryDataException.class, () -> {
+            service.updatePersonByDTOAndRemoveFromLegacyUsers(p, ""); 
+        });
+        assertTrue(e.getMessage().contains("SSN"));
+    }
+
+    @Test
+    void testThatTemporaryEmailCantExistWhenUpdatingPerson(){
+        Person p = dummyLegacyPerson(true, false);
+        Exception e = assertThrows(UpdatedPersonContainsTemporaryDataException.class, () -> {
+            service.updatePersonByDTOAndRemoveFromLegacyUsers(p, ""); 
+        });
+        assertTrue(e.getMessage().contains("email"));
+    }  
+
+    @Test
+    void testThatUpdatedUserCantUseUsernameAlreadyInUse(){
+        Person p1 = dummyPerson("iExist");
+        doReturn(p1).when(personRepo).findPersonByUsername("iExist");
+        Exception e = assertThrows(UsernameAlreadyExistsException.class, () -> {
+            service.updatePersonByDTOAndRemoveFromLegacyUsers(p1, ""); 
+        });
+        assertTrue(e.getMessage().contains("Username"));
+    }
+
+    @Test
+    void testThatUpdatedUserCantUseEmailAlreadyInUse(){
+        Person p1 = dummyPerson();
+        doReturn(p1).when(personRepo).findPersonByEmail(p1.getEmail());
+        Exception e = assertThrows(EmailAlreadyExistsException.class, () -> {
+            service.updatePersonByDTOAndRemoveFromLegacyUsers(p1, ""); 
+        });
+        assertTrue(e.getMessage().contains("Email"));
+    }
+
+    private void validationAssertions(Object expected, Object actual){
+        assertFalse(actual == null);
+        assertSame(expected,actual);
+    }
+
+    private Person dummyLegacyPerson(boolean tempEmail,  boolean tempSSN){
+        Random rand = new Random();
+        String email = String.format("fake%05d@fakemegafake.email", rand.nextInt(100000));
+        String SSN = String.format("000000%06d",rand.nextInt(1000000));
+        if(!tempEmail) email = String.format("email%06d@email.com",rand.nextInt(1000000));
+        if(!tempSSN) SSN = String.format("111111%06d",rand.nextInt(1000000));
+        return new Person("name", "surname", email, SSN, "user", "qooqWan123!");
+    }
+
+    /* ----------------------------------------------------------
+     * Impressive if a @Unique value is generated more than once.
+     * ----------------------------------------------------------
+     */
+    private Person dummyPerson(){
+        Random rand = new Random();
+        String username = String.format("username%06d", rand.nextInt(1000000));
+        return dummyPerson(username, "qooqWan123!");
+    }
+
+    private Person dummyPerson(String username){
+        return dummyPerson(username, "qooqWan123!");
+    }
+
+    private Person dummyPerson(String username, String password){
+        Random rand = new Random();
+        String ssn = String.format("111111%06d",rand.nextInt(1000000));
+        String email = String.format("email%06d@email.com",rand.nextInt(1000000));
+        return new Person("name", "surname", email, ssn, username, password); 
+    }
 
 }
